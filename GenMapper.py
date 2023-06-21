643d190780d22c213ce3fe3512a6cee609515b90
Levi Barnes <levi@nvidia.com>
2023-06-20 19:32:33 -0700

    Build all_ind list in GenMapper.py

--- GenMapper.py
+++ GenMapper.py
@@ -1,15 +1,36 @@
 from read_gedcom import read_gedcom
 from kmzcreate import create_kmz_file
+from kmzcreate import colordict
 
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 
+import os
+import re
+
 def generate_kmz(input_dir, output_file):
-    # Implement your generate_kmz routine here
-    # You can use the input_dir and output_file variables as needed
-    # For this example, we'll print the chosen input directory and output file path
-    print("Input directory:", input_dir)
+    #Find all the .ged files in the directory
+    all_files = os.listdir(input_dir)
+    gedre = re.compile(".*[.]ged$")
+    ged_files = [f for f in all_files if gedre.match(f)]
+    all_ind = []
+    colorind = 0
+    colorlist = list(colordict.values())
+    for f in ged_files:
+        print("Reading " + f)
+        ind = read_gedcom(input_dir + "/" + f)
+        color = colorlist[colorind]
+        colorind = colorind + 1
+        for i in ind:
+            i['color']=color
+        all_ind = all_ind + ind
+    print(all_ind[1000])
+    print(all_ind[2000])
+    #TODO geo-code locations in all_ind
+    #TODO convert dates in all_ind
+    #TODO extract important keys from all_ind into kmz_dots
+    #TODO call create_kmz_file(kmz_dots)
     print("KMZ output file:", output_file)
 
 def browse_input_dir():
@@ -31,34 +52,34 @@ def create_kmz():
     else:
         generate_kmz(input_dir, output_file)
         messagebox.showinfo("KMZ Created", "KMZ file created successfully.")
+if __name__ == "__main__":
+   # Create the main window
+   window = tk.Tk()
+   window.title("KMZ Generator")
 
-# Create the main window
-window = tk.Tk()
-window.title("KMZ Generator")
+   # Input directory
+   input_dir_label = tk.Label(window, text="Input directory:")
+   input_dir_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
 
-# Input directory
-input_dir_label = tk.Label(window, text="Input directory:")
-input_dir_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+   input_dir_entry = tk.Entry(window, width=50)
+   input_dir_entry.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W)
 
-input_dir_entry = tk.Entry(window, width=50)
-input_dir_entry.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W)
+   input_dir_button = tk.Button(window, text="Browse", command=browse_input_dir)
+   input_dir_button.grid(row=0, column=2, padx=10, pady=10)
 
-input_dir_button = tk.Button(window, text="Browse", command=browse_input_dir)
-input_dir_button.grid(row=0, column=2, padx=10, pady=10)
+   # KMZ output file
+   output_file_label = tk.Label(window, text="KMZ output:")
+   output_file_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
-# KMZ output file
-output_file_label = tk.Label(window, text="KMZ output:")
-output_file_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+   output_file_entry = tk.Entry(window, width=50)
+   output_file_entry.grid(row=1, column=1, padx=10, pady=10, sticky=tk.W)
 
-output_file_entry = tk.Entry(window, width=50)
-output_file_entry.grid(row=1, column=1, padx=10, pady=10, sticky=tk.W)
+   output_file_button = tk.Button(window, text="Browse", command=browse_output_file)
+   output_file_button.grid(row=1, column=2, padx=10, pady=10)
 
-output_file_button = tk.Button(window, text="Browse", command=browse_output_file)
-output_file_button.grid(row=1, column=2, padx=10, pady=10)
+   # Create button
+   create_button = tk.Button(window, text="Create", command=create_kmz)
+   create_button.grid(row=2, column=1, padx=10, pady=10)
 
-# Create button
-create_button = tk.Button(window, text="Create", command=create_kmz)
-create_button.grid(row=2, column=1, padx=10, pady=10)
-
-# Run the main event loop
-window.mainloop()
+   # Run the main event loop
+   window.mainloop()
---
+++ test.py
@@ -0,0 +1,4 @@
+from GenMapper import generate_kmz
+
+generate_kmz("C:/Users/lbarnes/Documents/GitHub/GenMapper/Joseph Chandler Files-20230621T003941Z-001/Joseph Chandler Files/Chandler GEDcom Files - February 17, 2018",
+             "C:/Users/lbarnes/Documents/GitHub/GenMapper/Chandler.kmz")
