7264978d29d773e15ed83febe57b8a29a17503a1
Levi Barnes <levi@nvidia.com>
2023-06-20 19:30:29 -0700

    Fixes for read_gedcom: only IND, fix SEX

--- read_gedcom.py
+++ read_gedcom.py
@@ -1,6 +1,9 @@
+import re
 def read_gedcom(file_path):
+    re_ind = re.compile(".*IND.*")
     individuals = []
     datemode = 'none'
+    skip = False
     with open(file_path, 'r') as file:
         individual = {}
         for line in file:
@@ -8,14 +11,18 @@ def read_gedcom(file_path):
             tag = line[2:6].strip()
             value = line[7:].strip()
             
-            if level == 0 and tag == 'INDI':
-                if individual:
+            if level == 0:
+                if individual and not skip:
                     individuals.append(individual)
-                individual = {'id': value}
+                if re_ind.match(value):
+                    skip = False
+                    individual = {'id': value}
+                else:
+                    skip = True
             elif level == 1 and tag == 'NAME':
                 individual['name'] = value
             elif level == 1 and tag == 'SEX':
-                individual['sex'] = value
+                individual['sex'] = line[6:].strip()
             elif level == 1 and tag == 'BIRT':
                 datemode = 'birth'
             elif level == 2 and tag == 'DATE' and datemode == 'birth':
@@ -28,10 +35,10 @@ def read_gedcom(file_path):
                 individual['deathdate'] = value
             elif level == 2 and tag == 'PLAC' and datemode == 'death':
                 individual['deathplace'] = value
-        if individual:
+        if individual and not skip:
             individuals.append(individual)
     return individuals
 
 if __name__ == "__main__":
     dots = read_gedcom("C:\\Users\\lbarnes\\Documents\\GitHub\\GenMapper\\Joseph Chandler Files-20230621T003941Z-001\\Joseph Chandler Files\\Chandler GEDcom Files - February 17, 2018\\John Jenkins and Leticia Anderson_2018-02-17.ged")
-    print(dots[0])
+    print(dots[16])
